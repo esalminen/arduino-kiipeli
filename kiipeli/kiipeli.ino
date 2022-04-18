9bc2410eea4a7414362e0efd89c5bd9922f38e34
Esa Salminen <esa_salminen@hotmail.com>
2022-04-18 11:52:16 +0300

    EEPROM phonebook progress

--- kiipeli/kiipeli.ino
+++ kiipeli/kiipeli.ino
@@ -3,14 +3,22 @@
 #include <RGBLed.h>
 #include <SoftwareSerial.h>
 
+// Phonebook for allowed numbers
+struct phoneBook {
+    String no1 = "+358406646131";
+    String no2 = "";
+    String no3 = "";
+    String no4 = "";
+    String no5 = "";
+  }
+
 // LCD display 
-const int rs = 12, en = 13, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const uint8_t rs = 12, en = 13, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Sim800L
 const uint8_t rx = 10, tx = 11;
 SoftwareSerial sim(10, 11);
-
 
 String rawTextSms = "";
 String textSms = "";
@@ -20,11 +28,22 @@ bool messageReadyForLCD = false;
 bool initActive = true;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Read EEPROM phonenumberStorage
+  EEPROM.get(phoneBook, 0);
 
+  // Start sim communication
+  sim.begin(9600);
+
+  // LCD configuration no. of columns and rows
+  lcd.begin(16,2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(int i = 0; i < 10; i++)
+  {
+    Serial.println(phonenumberStorage[i]);
+  }
+}
 
 }
